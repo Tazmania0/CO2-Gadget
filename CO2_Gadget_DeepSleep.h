739ca3a38f5c3ee3b873ae29c904cae2cf2b11e6
Tazmania0 <tzmail2000@gmail.com>
2026-05-05 23:04:51 +0300

    Fix E-Ink deep sleep partial redraw handling

--- CO2_Gadget_DeepSleep.h
+++ CO2_Gadget_DeepSleep.h
@@ -673,8 +673,16 @@ void handleBLEOnWake() {
 
 void handleDisplayReverseOnWake() {
 #if defined(SUPPORT_TFT) || defined(SUPPORT_OLED) || defined(SUPPORT_EINK)
+    RTC_DATA_ATTR static bool lastDisplayReverseOnWake = false;
+    RTC_DATA_ATTR static bool lastDisplayReverseOnWakeValid = false;
+    bool displayReverseChanged = !lastDisplayReverseOnWakeValid || (lastDisplayReverseOnWake != deepSleepData.displayReverseOnWake);
+
     displayReverse = deepSleepData.displayReverseOnWake;
-    setDisplayReverse(displayReverse);
+    if (displayReverseChanged) {
+        setDisplayReverse(displayReverse);
+        lastDisplayReverseOnWake = displayReverse;
+        lastDisplayReverseOnWakeValid = true;
+    }
     reverseButtons(displayReverse);
 #endif
 }
--- CO2_Gadget_EINK.h
+++ CO2_Gadget_EINK.h
@@ -396,7 +396,6 @@ void initDisplayFromDeepSleep(bool forceRedraw = false) {
     RTC_DATA_ATTR static bool firstBoot = true;
     SPI.begin(EPD_SCLK, EPD_MISO, EPD_MOSI);
     display.epd2.setBusyCallback(busyCallbackDeepSleep);  // register callback to be called during BUSY active time
-    setElementLocations();
     if (firstBoot) {
         forceRedraw = true;
         display.init(115200, true, RESETDURATION, false);
@@ -417,7 +416,8 @@ void initDisplayFromDeepSleep(bool forceRedraw = false) {
     setDisplayRotation(1);
     // display.setRotation(1);
     display.setFont(&SmallFont);
-    // display.setTextColor(GxEPD_BLACK);
+    display.setTextColor(GxEPD_BLACK);
+    setElementLocations();
     // display.setFullWindow();
     // display.setPartialWindow(0, 0, display.width(), display.height());
 #ifdef DEBUG_EINK
@@ -727,18 +727,23 @@ void displayShowValues(bool forceRedraw = false) {
 #endif
     }
 
+    bool drawAllElements = forceRedraw || isLowPowerMode;
+
     if (forceRedraw) {
         display.fillScreen(GxEPD_WHITE);
         display.clearScreen(GxEPD_WHITE);
+    } else if (isLowPowerMode) {
+        display.setPartialWindow(0, 0, display.width(), display.height());
+        display.fillScreen(GxEPD_WHITE);
     }
-    showCO2(co2, elementPosition.co2X, elementPosition.co2Y, forceRedraw);
-    showTemperature(temp, elementPosition.tempXValue, elementPosition.tempYValue, forceRedraw);
-    showHumidity(hum, elementPosition.humidityXValue, elementPosition.humidityYValue, forceRedraw);
+    showCO2(co2, elementPosition.co2X, elementPosition.co2Y, drawAllElements);
+    showTemperature(temp, elementPosition.tempXValue, elementPosition.tempYValue, drawAllElements);
+    showHumidity(hum, elementPosition.humidityXValue, elementPosition.humidityYValue, drawAllElements);
     showBatteryIcon(elementPosition.batteryIconX, elementPosition.batteryIconY, true);
-    showWiFiIcon(elementPosition.wifiIconX, elementPosition.wifiIconY, forceRedraw);
-    showMQTTIcon(elementPosition.mqttIconX, elementPosition.mqttIconY, forceRedraw);
-    showBLEIcon(elementPosition.bleIconX, elementPosition.bleIconY, forceRedraw);
-    showEspNowIcon(elementPosition.espNowIconX, elementPosition.espNowIconY, forceRedraw);
+    showWiFiIcon(elementPosition.wifiIconX, elementPosition.wifiIconY, drawAllElements);
+    showMQTTIcon(elementPosition.mqttIconX, elementPosition.mqttIconY, drawAllElements);
+    showBLEIcon(elementPosition.bleIconX, elementPosition.bleIconY, drawAllElements);
+    showEspNowIcon(elementPosition.espNowIconX, elementPosition.espNowIconY, drawAllElements);
 
     einkDisplayUpdateInProgress = true;
     display.display(true);  // Partial update
@@ -799,20 +804,25 @@ void displayShowValues(bool forceRedraw = false) {
 #endif
     }
 
+    bool drawAllElements = forceRedraw || isLowPowerMode;
+
     if (forceRedraw) {
         display.setFullWindow();
+        display.fillScreen(GxEPD_WHITE);
+    } else if (isLowPowerMode) {
+        display.setPartialWindow(0, 0, display.width(), display.height());
         display.fillScreen(GxEPD_WHITE);
     }
 
     // testRedrawValues(true);
-    showCO2(co2, elementPosition.co2X, elementPosition.co2Y, forceRedraw);
-    showTemperature(temp, elementPosition.tempXValue, elementPosition.tempYValue, forceRedraw);
-    showHumidity(hum, elementPosition.humidityXValue, elementPosition.humidityYValue, forceRedraw);
+    showCO2(co2, elementPosition.co2X, elementPosition.co2Y, drawAllElements);
+    showTemperature(temp, elementPosition.tempXValue, elementPosition.tempYValue, drawAllElements);
+    showHumidity(hum, elementPosition.humidityXValue, elementPosition.humidityYValue, drawAllElements);
     showBatteryIcon(elementPosition.batteryIconX, elementPosition.batteryIconY, true);
-    showWiFiIcon(elementPosition.wifiIconX, elementPosition.wifiIconY, forceRedraw);
-    showMQTTIcon(elementPosition.mqttIconX, elementPosition.mqttIconY, forceRedraw);
-    showBLEIcon(elementPosition.bleIconX, elementPosition.bleIconY, forceRedraw);
-    showEspNowIcon(elementPosition.espNowIconX, elementPosition.espNowIconY, forceRedraw);
+    showWiFiIcon(elementPosition.wifiIconX, elementPosition.wifiIconY, drawAllElements);
+    showMQTTIcon(elementPosition.mqttIconX, elementPosition.mqttIconY, drawAllElements);
+    showBLEIcon(elementPosition.bleIconX, elementPosition.bleIconY, drawAllElements);
+    showEspNowIcon(elementPosition.espNowIconX, elementPosition.espNowIconY, drawAllElements);
     // display.hibernate();
 
     if (forceRedraw) {
